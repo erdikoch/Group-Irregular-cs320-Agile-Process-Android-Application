dde17c0c5a0ff1a19d648d96376e682d5f75e147
gamzekucukcolak <gamze.kucukcolak@ozu.edu.tr>
2014-05-05 19:54:22 +0300

    SettingsActivity.java

---
+++ W.A.P/src/com/GroupIrregular/W.A.P/SettingsActivity.java
@@ -0,0 +1,44 @@
+package com.groupIrregular.wap;
+
+import java.util.List;
+
+import com.kodlab.kimnerede.R;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.MenuItem;
+
+public class SettingsActivity extends PreferenceActivity {
+	
+	@Override
+	public void onBuildHeaders(List<Header> target) {
+		loadHeadersFromResource(R.xml.preferences, target);
+	}
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        
+    }
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    
+		switch (item.getItemId()) {
+	        case android.R.id.home:
+	            Intent intent = new Intent(this, MenuActivity.class);
+	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	            startActivity(intent);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+		
+	}
+
+}
